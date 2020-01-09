eae82b622549c2d40c1ed7f134472281f19f439b
ggruenhagen3 <ggruenhagen3@gatech.edu>
2020-01-09 13:04:52 -0500

    Debugging

--- search_reads.py
+++ search_reads.py
@@ -165,7 +165,7 @@ def main():
         output = []
         for file in os.listdir(dir):
             if file.endswith(".bam"):
-                this_output = subprocess.check_output(["samtools view", "-F 0x04",  "-q 30", str(dir) + "/" + file, coord])
+                this_output = subprocess.check_output(["samtools", "view", "-F", "0x04", "-q", "30", str(dir) + "/" + file, coord])
                 output_lines = this_output.decode().split("\n")
                 len_output_lines = len(output_lines) - 1  # -1 because the last one is empty string
                 output.extend(output_lines[:-1])
