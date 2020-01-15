cd2e8ea75bb292209bea6cf0fafd71470a5d0d9d
ggruenhagen3 <ggruenhagen3@gatech.edu>
2020-01-15 14:33:08 -0500

    Debugging

--- blast_filter.py
+++ blast_filter.py
@@ -26,9 +26,9 @@ def readGTF(gtf):
     with open(gtf, 'r') as input:
         for line in input:
             lineSplit = line.split()
-            id = lineSplit[8][9:27]
+            id = lineSplit[7][9:27]
             gtfDict[str(lineSplit[0]) + ":" + str(int(lineSplit[3])+1) + "-" + str(lineSplit[4])] = id
-            print(id)
+            # print(id)
             if id == "ENSMZEG00005000039":
                 print(str(lineSplit[0]) + ":" + str(int(lineSplit[3])+1) + "-" + str(lineSplit[4]))
 
