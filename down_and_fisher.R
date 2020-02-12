7ae35e04d54963d58e7b2edd352962f9c7801d41
ggruenhagen3 <ggruenhagen3@gatech.edu>
2020-02-12 16:46:18 -0500

    Downsample then do Fisher's Exact Test

---
+++ down_and_fisher.R
@@ -0,0 +1,119 @@
+library("dplyr")
+library("Matrix")
+library("Seurat")
+library("stringr")
+
+geneCap <- function(gene, gene_names) {
+  # Gene the gene name in the right format
+  gene_lower <- tolower(gene)
+  gene_upper <- toupper(gene)
+  gene_title <- str_to_title(gene)
+  error <- FALSE
+  if (gene_lower %in% gene_names) {
+    gene <- gene_lower
+  } else if (gene_upper %in% gene_names) {
+    gene <- gene_upper
+  } else if (gene_title %in% gene_names) {
+    gene <- gene_title
+  } else {
+    error <- TRUE
+  }
+  
+  return(c(gene, error))
+}
+
+validGenes <- function(genes, gene_names) {
+  valid_genes <- c()
+  for (gene in genes) {
+    result <- geneCap(gene, gene_names)
+    gene <- result[1]
+    error <- as.logical(result[2])
+    if (! error) {
+      valid_genes <- c(valid_genes, gene)
+    }
+  } # end gene for
+  
+  return(valid_genes)
+} # end validGenes function
+
+downsample <- function(combined, marker_genes, run) {
+  set.seed(run)
+  min_trans <- min(combined$nCount_RNA)
+  gene_names <- rownames(combined@assays$RNA@counts)
+  # new_matrix <- matrix(, nrow = nrow(combined@assays$RNA@counts), ncol = ncol(combined@assays$RNA@counts), dimnames = list(gene_names, colnames(combined@assays$RNA@counts)))
+  # new_new_matrix <- matrix(, nrow=nrow(combined@assays$RNA@counts))
+  marker_matrix  <- matrix(, nrow=length(marker_genes), ncol = ncol(combined@assays$RNA@counts), dimnames = list(marker_genes, colnames(combined@assays$RNA@counts)))
+  i <- 0
+  for (cell in colnames(combined@assays$RNA@counts)) {
+    # i <- i + 1
+    # if (i%%500 == 1) {
+    #   print(cell)
+    # }
+    # start.time <- Sys.time()
+    
+    trans_names <- rep(gene_names, combined@assays$RNA@counts[,cell])
+    ran_trans_names <- sample(trans_names, min_trans)
+    ran_trans_names <- ran_trans_names[which(ran_trans_names %in% marker_genes)]
+    ran_df <- as.data.frame(table(ran_trans_names))
+    zero_gene_names <- marker_genes[which(! marker_genes %in% ran_trans_names)]
+    zero_df <- setNames(data.frame(zero_gene_names <- zero_gene_names, Freq <- rep(0, length(zero_gene_names))), c("ran_trans_names", "Freq"))
+    ran_df <- rbind(ran_df, zero_df)
+    rownames(ran_df) <- ran_df$ran_trans_names
+    ran_df <- ran_df[marker_genes,2]
+    # new_matrix[,cell] <-as.matrix(ran_df)
+    
+    
+    marker_matrix[,cell] <- as.matrix(ran_df)
+    # new_new_matrix <- cbind(new_new_matrix, as.matrix(ran_df))
+    
+    # end.time <- Sys.time()
+    # time.taken <- end.time - start.time
+    # print(time.taken)
+  }
+  
+  return(marker_matrix)
+}
+## END FUNCTIONS ##
+rna_path <- "~/scratch/brain/"
+combined <- readRDS(paste(rna_path, "/brain_scripts/brain_shiny/data/combined.rds", sep = ""))
+marker_path <- paste(rna_path, "data/markers/", sep="")
+marker_files <- dir(marker_path, pattern =paste("*.txt", sep=""))
+
+markers <- data.frame(gene <- c(), bio <- c())
+for (i in 1:length(marker_files)) {
+  file <- read.table(paste(marker_path, marker_files[i], sep=""), header=FALSE, sep="\t", stringsAsFactors=FALSE)
+  file[,1] <- toupper(file[,1])
+  markers <- rbind(markers, file[,1:2])
+}
+colnames(markers) <- c("gene", "bio")
+markers <- markers[which(markers$bio == "DISC_ASE"),]
+gene_names <- rownames(combined@assays$RNA)
+marker_genes <- validGenes(markers$gene, gene_names)
+num_clusters <- as.numeric(tail(levels(combined@meta.data$seurat_clusters), n=1))
+
+# Bootstrap
+total_genes_per_cluster <- rep(0, num_clusters+1)
+for (run in 1:50) {
+  print(run)
+  mat <- downsample(combined, marker_genes, run)
+  
+  cells_per_cluster <- c()
+  genes_per_cluster <- c()
+  for (i in 0:num_clusters) {
+    this_cells <- WhichCells(combined, idents = i)
+    # genes_per_cluster <- c(genes_per_cluster, length(which(as.vector(combined@assays$RNA@counts[ran_markers,this_cells]) != 0))) # genes
+    genes_per_cluster <- c(genes_per_cluster, length(which(as.vector(mat[,this_cells]) != 0))) # genes
+    cells_per_cluster <- c(cells_per_cluster, length(this_cells))
+  }
+  total_genes_per_cluster <- total_genes_per_cluster + genes_per_cluster
+}
+
+avg_genes_per_cluster <- total_genes_per_cluster/50
+all <- t(sum(total_genes_per_cluster), sum(cells_per_cluster))
+results <- data.frame()
+for (i in 0:num_clusters) {
+  fisher_p <- fisher.test(data.frame(cluster <- t(c(avg_genes_per_cluster[i+1],cells_per_cluster[i+1])), all <- all))$p.value
+  results <- rbind(results, t(c(i, avg_genes_per_cluster[i+1]/cells_per_cluster[i+1], fisher_p)))
+}
+print(all[1]/all[2])
+write.table(results, file = paste(rna_path, "/results/down_and_fisher.tsv", sep=""), sep = "\t", row.names = FALSE, quote=FALSE)
