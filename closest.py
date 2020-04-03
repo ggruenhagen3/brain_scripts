ffe75ffa7c1b180e969a48361f88b9a85180fc45
ggruenhagen3 <ggruenhagen3@gatech.edu>
2020-04-03 14:53:10 -0400

    Find closest

--- closest.py
+++ closest.py
@@ -45,7 +45,7 @@ def main():
     print("Calling snpEff")
     cwd = os.getcwd()
     os.chdir("/nv/hp10/cpatil6/genomics-shared/snpEff/")
-    out = subprocess.Popen(["java", "-jar", "snpEff.jar", "closest", "Mzebra", cwd + "tmp.vcf"], stdout=subprocess.PIPE)
+    out = subprocess.Popen(["java", "-jar", "snpEff.jar", "closest", "Mzebra", cwd + "/tmp.vcf"], stdout=subprocess.PIPE)
     print(out.communicate()[1][0:50])
     os.chdir(cwd)
     print("Done")
