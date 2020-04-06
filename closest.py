563cb156612813a6b9d2fc97766ce59c955493d4
ggruenhagen3 <ggruenhagen3@gatech.edu>
2020-04-06 12:31:49 -0400

    Debugging

--- closest.py
+++ closest.py
@@ -111,7 +111,7 @@ def main():
     snpEff_dir = "/nv/hp10/cpatil6/genomics-shared/snpEff/"
     os.chdir(snpEff_dir)
     # os.chdir("/nv/hp10/cpatil6/genomics-shared/snpEff/")
-    out = subprocess.Popen(["java", "-jar", "snpEff.jar", "closest", "-geneId", "Mzebra_ENS", cwd + "/tmp.vcf"], stdout=subprocess.PIPE)
+    out = subprocess.Popen(["java", "-jar", "snpEff.jar", "closest", "Mzebra_ENS", cwd + "/tmp.vcf"], stdout=subprocess.PIPE)
     output = out.communicate()[0]
     os.chdir(cwd)
     print("Done\n")
