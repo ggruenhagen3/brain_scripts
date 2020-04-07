ae19b5e45b874511ea2044d45bb73a79176984be
ggruenhagen3 <ggruenhagen3@gatech.edu>
2020-04-07 13:36:34 -0400

    Mouse Cortex and hippocampus

--- m_cor_hip.R
+++ m_cor_hip.R
@@ -27,13 +27,33 @@ original_df <- inner_join(tsne_coordinates, metadata, by = "sample_name")
 head(original_df)
 # original_df <- cbind(tsne_coordinates[2:3], metadata$cluster_color)
 # colnames(original_df) <- c("tsne_1", "tsne_2", "cluster_color")
-png(paste(tome_path, "original_tsne.png", sep=""), width = 7200, height = 7200, unit = "px")
-ggplot(original_df, aes(tsne_1, tsne_2, color=cluster_color)) + geom_point()
+png(paste(tome_path, "original_tsne.png", sep=""), width = 4800, height = 4800, unit = "px")
+ggplot(original_df, aes(tsne_1, tsne_2, color=cluster_color)) + geom_point() + theme_classic()
 dev.off()
 
-
 # Redo Clustering in Seurat
-# exons <- CreateSeuratObject(counts = exons, project = "EXON")
-# introns <- CreateSeuratObject(counts = introns, project = "INTRON")
-# exons$cond <- "EXON"
-# introns$cond <- "INTRON"
+exons <- CreateSeuratObject(counts = exons, project = "EXON")
+introns <- CreateSeuratObject(counts = introns, project = "INTRON")
+exons$cond <- "EXON"
+introns$cond <- "INTRON"
+exons <- NormalizeData(exons, normalization.method = "LogNormalize", scale.factor = 100000)
+introns <- NormalizeData(introns, normalization.method = "LogNormalize", scale.factor = 100000)
+m_cor_hip <- merge(exons, introns, merge.data = TRUE)
+cat(paste("Number of rows in metadata:", nrow(metadata)))
+cat(paste("Number of rows in m_cor_hip (no subset):", nrow(metadata)))
+m_cor_hip <- subset(m_cor_hip, subset = nFeature_RNA > 500)
+m_cor_hip <- FindVariableFeatures(object = m_cor_hip, mean.function = ExpMean, dispersion.function = LogVMR, nfeatures = 2000)
+m_cor_hip <- ScaleData(object = m_cor_hip, vars.to.regress = NULL)
+m_cor_hip <- RunPCA(m_cor_hip, npcs = 50, verbose = FALSE)
+m_cor_hip <- RunTSNE(object = m_cor_hip)
+m_cor_hip <- RunUMAP(m_cor_hip, reduction = "pca", dims = 1:20)
+m_cor_hip <- FindNeighbors(m_cor_hip, reduction = "umap", dims = 1:2)
+m_cor_hip <- FindClusters(m_cor_hip, resolution = 0.1)
+
+png(paste(tome_path, "new_umap.png", sep=""), width = 4800, height = 4800, unit = "px")
+DimPlot(m_cor_hip, reduction = "umap", label = TRUE)
+dev.off()
+
+png(paste(tome_path, "new_tsne.png", sep=""), width = 4800, height = 4800, unit = "px")
+DimPlot(m_cor_hip, reduction = "tsne", label = TRUE)
+dev.off()
