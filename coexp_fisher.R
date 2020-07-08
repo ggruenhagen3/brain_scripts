d65ec13b1f7f10ae473415d6812acda6eb7f7b2a
ggruenhagen3 <ggruenhagen3@gatech.edu>
2020-07-08 09:20:59 -0400

    Fixed typo

--- coexp_fisher.R
+++ coexp_fisher.R
@@ -111,7 +111,7 @@ for (col in 2:length(gene_names)) {
     
     # Do a Fisher's Exact Test, where you test the ratio of positive gene1 cells
     # in gene2 cells compared to the number of positive cell gene1 cells in not gene 2 cells.
-    contig_table <- data.frame(c(length(ovlp), length(gene_2_cells)), c(length(gene_1_cells), length(non_gene_2_cells)))
+    contig_table <- data.frame(c(length(ovlp), length(gene_2_cells)), c(length(gene_1_cells), length(not_gene_2_cells)))
     mat_fish[row,col] = fisher.test(contig_table)$p.value
   }
 }
