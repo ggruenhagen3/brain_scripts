9641365b4395c58dc64277dc8c810ff5b1892ce7
ggruenhagen3 <ggruenhagen3@gatech.edu>
2020-07-14 11:13:41 -0400

    parallelization

--- test.R
+++ test.R
@@ -6,4 +6,8 @@ library("jaccard")
 lncRNA <- readRDS("/nv/hp10/ggruenhagen3/scratch/brain/data/lncRNA.RDS")
 obj <- lncRNA
 gene_names <- rownames(obj)[which(rowSums(as.matrix(obj@assays$RNA@counts)) != 0)]
+test = obj@assays$RNA@data
+print("Parallelized:")
 system.time(cor.pvalues <- future_sapply(2:100, function(x) { sapply(1:(x-1), function(y) { cor.test(test[gene_names[x],],test[gene_names[y],])$p.value })}))
+print("Normal:")
+system.time(cor.pvalues <- sapply(2:100, function(x) { sapply(1:(x-1), function(y) { cor.test(test[gene_names[x],],test[gene_names[y],])$p.value })}))
