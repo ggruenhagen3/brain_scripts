9615327442f1fda41e419565c2adb5bd4f57e772
ggruenhagen3 <ggruenhagen3@gatech.edu>
2020-07-15 16:19:40 -0400

    The real thing

--- cor_pace.R
+++ cor_pace.R
@@ -7,8 +7,8 @@ library("doParallel")
 lncRNA <- readRDS("/nv/hp10/ggruenhagen3/scratch/brain/data/lncRNA.RDS")
 obj <- lncRNA
 gene_names <- rownames(obj)[which(rowSums(as.matrix(obj@assays$RNA@counts)) != 0)]
-mat_data_p   = matrix(0, nrow=length(gene_names), ncol = length(gene_names), dimnames = list(gene_names, gene_names))
-mat_data_cor = matrix(0, nrow=length(gene_names), ncol = length(gene_names), dimnames = list(gene_names, gene_names))
+# mat_data_p   = matrix(0, nrow=length(gene_names), ncol = length(gene_names), dimnames = list(gene_names, gene_names))
+# mat_data_cor = matrix(0, nrow=length(gene_names), ncol = length(gene_names), dimnames = list(gene_names, gene_names))
 # test = as.matrix(t(obj@assays$RNA@data))
 test = obj@assays$RNA@data
 ptm <- proc.time()
@@ -29,7 +29,7 @@ df_p  = foreach(col = 2:500, .combine='rbind') %dopar% {
   thisRow
 }
 # One for correlation
-df_p  = foreach(col = 2:500, .combine='rbind') %dopar% {
+df_p  = foreach(col = 2:length(gene_names), .combine='rbind') %dopar% {
   gene1 = gene_names[col]
   thisRow = c()
   for ( row in 1:(col-1) ) {
@@ -44,20 +44,8 @@ df_p  = foreach(col = 2:500, .combine='rbind') %dopar% {
 }
 stopCluster(cl)
 proc.time() - ptm
-# saveRDS(df_p,   "/nv/hp10/ggruenhagen3/scratch/brain/data/df_p.RDS")
-# saveRDS(df_cor, "/nv/hp10/ggruenhagen3/scratch/brain/data/df_cor.RDS")
-
-ptm <- proc.time()
-for (col in 2:500) {
-  gene1 = gene_names[col]
-  for ( row in 1:(col-1) ) {
-    gene2 = gene_names[row]
-    cor_res = cor.test(obj@assays$RNA@data[gene1,], obj@assays$RNA@data[gene2,])
-    mat_data_p[row,col]   = cor_res$p.value
-    mat_data_cor[row,col] = cor_res$estimate
-  }
-}
-proc.time() - ptm
+saveRDS(df_p,   "/nv/hp10/ggruenhagen3/scratch/brain/data/df_p.RDS")
+saveRDS(df_cor, "/nv/hp10/ggruenhagen3/scratch/brain/data/df_cor.RDS")
 
 # for (col in 2:length(gene_names)) {
 #   if (col %% 100 == 0) {
