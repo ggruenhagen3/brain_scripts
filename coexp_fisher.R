9c7c7a920701a971c9ae3dc3d5bef95c63d7312d
ggruenhagen3 <ggruenhagen3@gatech.edu>
2020-08-06 10:34:24 -0400

    Fixed not_gene_2_cells

--- coexp_fisher.R
+++ coexp_fisher.R
@@ -108,7 +108,7 @@ for (col in 2:length(gene_names)) {
     gene_2_cells = gene_cells[[gene2]]
     ovlp = gene_1_cells[which(gene_1_cells %in% gene_2_cells)]
     not_ovlp = gene_1_cells[which(! gene_1_cells %in% gene_2_cells)]
-    not_gene_2_cells = all_cells[which(! gene_2_cells %in% all_cells)]
+    not_gene_2_cells = all_cells[which(! all_cells %in% gene_2_cells)]
     
     # Do a Fisher's Exact Test, where you test the ratio of positive gene1 cells
     # in gene2 cells compared to the number of positive cell gene1 cells in not gene 2 cells.
