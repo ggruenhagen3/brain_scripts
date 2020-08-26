bc8722341adb16c5d9a207f73e2a0d1292ed7e74
ggruenhagen3 <ggruenhagen3@gatech.edu>
2020-08-26 10:48:15 -0400

    update

--- convert_scaffolds.py
+++ convert_scaffolds.py
@@ -2,7 +2,7 @@ import argparse
 import re
 
 def parseArgs():
-    parser = argparse.ArgumentParser(description='Search reads for SNPs')
+    parser = argparse.ArgumentParser(description='Converts Linkage Group Names to NCBI Format with NC and vice versa')
     parser.add_argument('input', metavar='i', help='Input File')
     parser.add_argument('output', metavar='o', help='Name of Output File')
     parser.add_argument("-l", "--lg_to_nc", help="Converts LG to NC", action="store_true")
@@ -18,18 +18,32 @@ def readFile(file):
             lines.append(line)
     return lines
 
+def readAssemblyReport(assemblyReportPath):
+    dict = {}
+    with open(assemblyReportPath, 'r') as input:
+        for line in input:
+            if not line.startswith("#"):
+                lineSplit = line.split("\t")
+                lg = lineSplit[2]
+                dict[lineSplit[6]] = lg
+                if lg == "na":
+                    dict[lineSplit[6]] = lineSplit[4]
+    return dict
 
-def convertScaffolds(lines, toNC):
+
+def convertScaffolds(lines, toNC, toLG):
     """"
     From a list of lines, use regex to convert from LG to NC or NC to LG
     """
     new_lines =[]
-    dict = {}
-    for i in range(1, 21):
-        # dict["LG" + str(i)] = "NC_0" + str(i + 36779.1)
-        dict["NC_0" + str(float(i) + float(36779.1))] = "LG" + str(i) # key is NC_ and value is LG
-    dict["NC_036800.1"] = "LG22"
-    dict["NC_036801.1"] = "LG23"
+    # dict = {}
+    # for i in range(1, 21):
+    #     # dict["LG" + str(i)] = "NC_0" + str(i + 36779.1)
+    #     dict["NC_0" + str(float(i) + float(36779.1))] = "LG" + str(i) # key is NC_ and value is LG
+    # dict["NC_036800.1"] = "LG22"
+    # dict["NC_036801.1"] = "LG23"
+    assemblyReportPath = "/mnt/c/Users/miles/Downloads/all_research/M_zebra_UMD2a_assembly_report.txt"
+    dict = readAssemblyReport(assemblyReportPath)
 
     for line in lines:
         for key in dict.keys():
