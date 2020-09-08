f99357259b77ddd104b3819d3fab55552ec5d247
ggruenhagen3 <ggruenhagen3@gatech.edu>
2020-09-08 17:40:37 -0400

    Multi heatmap

--- all_f.R
+++ all_f.R
@@ -681,6 +681,7 @@ heatmapComparisonMulti = function(dfs, samples, filename, filepath) {
     clusters[[i]] = unique(as.vector(dfs[[i]]$cluster))
     num_clusters[[i]] = length(clusters[i])
     all_logFC = c(all_logFC, dfs[[i]]$avg_logFC)
+    print(clusters[[i]])
   }
   
   # Now do Pairwise Comparison of each df's DEGs
