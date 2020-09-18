ead377153d2b5bc9910a0c0f9882d2699b62b4cc
ggruenhagen3 <ggruenhagen3@gatech.edu>
2020-09-18 17:25:48 -0400

    Dendrogram updat

--- all_f.R
+++ all_f.R
@@ -902,12 +902,17 @@ heatmapComparisonMulti = function(dfs, samples, filename, filepath, correction_f
     print(paste("Creating Dendrogram Matrix of size", nrow(dend_mat), "x", ncol(dend_mat)))
     print(head(all_genes))
     j = 0
+    # for (i in 1:length(dfs)) {
+    #   for (i_clust in 1:num_clusters[[i]]) {
+    #     i_clust_df = dfs[[i]][which(dfs[[i]]$cluster == clusters[[i]][i_clust]),]
+    #     i_clust_df = i_clust_df[!duplicated(i_clust_df$gene),]
+    
     for (i in 1:length(dfs)) {
       print(i)
       for (i_clust in 1:num_clusters[[i]]) {
         i_clust_df = dfs[[i]][which(dfs[[i]]$cluster == clusters[[i]][i_clust]),]
-        i_clust_df = i_clust_df[!duplicated(i_clust_df$gene),]
         print(head(i_clust_df$gene))
+        i_clust_df = i_clust_df[!duplicated(i_clust_df$gene),]
         gene_in_all_genes = i_clust_df$gene[which(i_clust_df$gene %in% all_genes)]
         print(length(gene_in_all_genes))
         dend_mat[gene_in_all_genes, j] = i_clust_df$avg_logFC[which(i_clust_df$gene %in% all_genes)]
