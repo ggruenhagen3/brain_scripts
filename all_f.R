72b5a61d3ec926fc2fe90dae2e66309e5190fab7
ggruenhagen3 <ggruenhagen3@gatech.edu>
2020-09-18 17:16:53 -0400

    Dendrogram updat

--- all_f.R
+++ all_f.R
@@ -907,7 +907,9 @@ heatmapComparisonMulti = function(dfs, samples, filename, filepath, correction_f
         i_clust_df = dfs[[i]][which(dfs[[i]]$cluster == clusters[[i]][i_clust]),]
         i_clust_df = i_clust_df[!duplicated(i_clust_df$gene),]
         gene_in_all_genes = i_clust_df$gene[which(i_clust_df$gene %in% all_genes)]
+        print(length(gene_in_all_genes))
         dend_mat[gene_in_all_genes, j] = i_clust_df$avg_logFC[which(i_clust_df$gene %in% all_genes)]
+        print(i_clust_df$avg_logFC[which(i_clust_df$gene %in% all_genes)])
         j = j + 1
       }
     }
