65a015a29a0f0df8af05ac2276178c1d62c803ab
ggruenhagen3 <ggruenhagen3@gatech.edu>
2020-09-21 09:53:47 -0400

    Dendrogram updat

--- all_f.R
+++ all_f.R
@@ -911,12 +911,13 @@ heatmapComparisonMulti = function(dfs, samples, filename, filepath, correction_f
     for (i in 1:length(dfs)) {
       print(i)
       print(head(dfs[[i]]))
-      for (i_clust in 1:num_clusters[[i]]) {
+      for (i_clust in dfs[[i]]$cluster) {
         print(i_clust)
-        print(clusters[[i]])
-        print(clusters[[i]][i_clust])
-        print(clusters[[i]][i_clust] %in% dfs[[i]]$cluster)
-        i_clust_df = dfs[[i]][which(dfs[[i]]$cluster == clusters[[i]][i_clust]),]
+        # print(clusters[[i]])
+        # print(clusters[[i]][i_clust])
+        # print(clusters[[i]][i_clust] %in% dfs[[i]]$cluster)
+        # i_clust_df = dfs[[i]][which(dfs[[i]]$cluster == clusters[[i]][i_clust]),]
+        i_clust_df = dfs[[i]][which(dfs[[i]]$cluster == i_clust),]
         print(head(i_clust_df))
         # print(head(i_clust_df$gene))
         i_clust_df = i_clust_df[!duplicated(i_clust_df$gene),]
