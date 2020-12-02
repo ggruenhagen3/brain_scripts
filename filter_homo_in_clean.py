ad9a2cb0e1a26242aa7a04be12395efd4696407c
George Gruenhagen <gwg@gatech.edu>
2020-12-02 15:20:49 -0500

    Filter out SNPs that are homo in clean cellranger reads

--- filter_homo_in_clean.py
+++ filter_homo_in_clean.py
@@ -68,9 +68,9 @@ def keepLines(snp, dir, outputFile, barcodes):
             if file.endswith("b1.bam"):
                 # this_output = subprocess.check_output(["samtools", "view", "-F", "0x04", "-q", "30", str(dir) + "/" + file, coord])
                 this_output = subprocess.check_output(["samtools", "view", "-F", "4", str(dir) + "/" + file, coord])
-                print(this_output)
                 output_lines = this_output.decode().split("\n")
                 len_output_lines = len(output_lines) - 1  # -1 because the last one is empty string
+                print(len_output_lines)
                 # output_lines = []
                 output.extend(output_lines[:-1])
         # output = filterCIGAR(output)
