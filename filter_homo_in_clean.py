8731ea17cf6f07c7fdfc769b77553899a10bf330
George Gruenhagen <gwg@gatech.edu>
2020-12-02 19:56:17 -0500

    Filter out SNPs that are homo in clean cellranger reads

--- filter_homo_in_clean.py
+++ filter_homo_in_clean.py
@@ -85,8 +85,8 @@ def keepLines(snp, dir, outputFile, barcodes):
                 # this_output = subprocess.check_output(["samtools", "view", "-F", "4", str(dir) + "/" + file, coord])
                 output_lines = this_output.decode().split("\n")
                 filtered_output_lines = filterCellranger(output_lines, barcodes[file.split(".")[0]])
-                print(file.split(".")[0])
-                print(len(filtered_output_lines))
+                # print(file.split(".")[0])
+                # print(len(filtered_output_lines))
                 output.extend(filtered_output_lines)
                 # len_output_lines = len(output_lines) - 1  # -1 because the last one is empty string
                 # output.extend(output_lines[:-1])
@@ -107,8 +107,8 @@ def readBarcodes(barcodes_dir):
 
 def main():
     snp_file, dir, verbose, outputFile, barcodes = parseArgs()
-    # snp = ["NC_036780.1:118274-118845", "NC_036780.1:166532-244697", "NC_036780.1:272743-279989", "NC_036780.1:332525-366704", "NC_027944.1:14412-15552"]
-    snp = ["NC_036780.1:272743-279989"]
+    snp = ["NC_036780.1:118274-118845", "NC_036780.1:166532-244697", "NC_036780.1:272743-279989", "NC_036780.1:332525-366704", "NC_027944.1:14412-15552"]
+    # snp = ["NC_036780.1:272743-279989"]
     barcodes = readBarcodes(barcodes)
     keepLines(snp, dir, outputFile, barcodes)
 
