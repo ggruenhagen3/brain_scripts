0dac019744f8e5a555b0491acad219517410d73c
George Gruenhagen <gwg@gatech.edu>
2020-12-03 16:35:58 -0500

    Filter out SNPs that are homo in clean cellranger reads

--- filter_homo_in_clean.py
+++ filter_homo_in_clean.py
@@ -35,56 +35,53 @@ def readSNP(snp_file):
             snp[coord] = line
     return snp
 
-# def filterCIGAR(lines):
-#     good_lines = []
-#     for line in lines:
-#         lineSplit = line.split()
-#         cigar = lineSplit[5]
-#         if cigar == "98M":
-#             good_lines.append(line)
-#     return good_lines
 
-def filterCellrangerRead(line, barcodes):
-    lineSplit = line.split("\t")
+def filterCellrangerRead(this_read, barcodes):
+    """
+    Determine whether the input read meets the Cellrnager criteria to be counted
+    :param this_read: a read in string format from pysam
+    :return: True/False based on if it meets the criteria
+    """
+    lineSplit = this_read.split("\t")
     info = lineSplit[11]
     if "('xf', 25)" in info and "CB" in info and "GN" in info and lineSplit[4] == "255":
-        barcode = line.split("'CB'")[1].split("'")[1]
-        genes = line.split("'GN'")[1].split(")")[0]
+        barcode = this_read.split("'CB'")[1].split("'")[1]
+        genes = this_read.split("'GN'")[1].split(")")[0]
         if barcode in barcodes and ";" not in genes:
             return True
     return False
 
-def filterCellranger(lines, barcodes):
-    good_lines = []
-    test = 0
-    for line in lines:
-        # if "xf:i:25" in line:
-        #     barcode = line.split("CB:Z:")[1].split()[0]
-        #     genes = line.split("GN:Z:")[1].split()[0]
-        #     print(line)
-        #     print(barcode)
-        #     print(barcode in barcodes)
-        #     print(genes)
-        #     print(";" not in genes)
-        #     test += 1
-        #     if test > 6:
-        #         break
-        if "xf:i:25" in line and "CB:Z:" in line and "GN:Z:" in line and line.split()[4] == "255":
-            barcode = line.split("CB:Z:")[1].split()[0]
-            genes = line.split("GN:Z:")[1].split()[0]
-            if barcode in barcodes and ";" not in genes:
-            # if barcode in barcodes and "GN:Z:dhcr7" in line:
-                good_lines.append(line)
-
-    return good_lines
+# def filterCellranger(lines, barcodes):
+#     """
+#     """
+#     good_lines = []
+#     test = 0
+#     for line in lines:
+#         if "xf:i:25" in line and "CB:Z:" in line and "GN:Z:" in line and line.split()[4] == "255":
+#             barcode = line.split("CB:Z:")[1].split()[0]
+#             genes = line.split("GN:Z:")[1].split()[0]
+#             if barcode in barcodes and ";" not in genes:
+#                 good_lines.append(line)
+#
+#     return good_lines
 
 def writeFile(file, lines):
+    """
+    Write some lines to a file
+    """
     f = open(file, "w+")
     for line in lines:
         f.write(line)
     f.close()
 
 def isHet(snp, samfiles, barcodes):
+    """
+    Determine whether a SNP is heterozygous in the subset of reads that meet Cellranger criteria in a list of bams.
+    :param snp: the SNP in question
+    :param samfiles: dictionary of samfiles in pysam format. Key is sample and value is pysam object.
+    :param barcodes: dictionary of barcodes that are not filtered out in bb. Key is sample and value is list of barcodes.
+    :return snp_is_het: True/False snp is heterozygous
+    """
     snp_pos = int(snp.split("-")[1])
     scaffold = snp.split(":")[0]
     pos = int(snp.split("-")[1])
@@ -108,6 +105,14 @@ def isHet(snp, samfiles, barcodes):
     return snp_is_het
 
 def keepLinesPysam(snp, dir, barcodes):
+    """
+    Determine which snps from the list are heterozygous in the subset of reads that meet Cellranger criteria in a
+    list of bams.
+    :param snp: list of snps
+    :param dir: directory of bam files
+    :param barcodes: dictionary of barcodes that are not filtered out in bb. Key is sample and value is list of barcodes
+    :return good_snp: list of snps that are heterozygous
+    """
     good_snp = []
     snp_coords = list(snp.keys())
     samfiles = {}  # key is sample and value is samfile object
@@ -125,38 +130,46 @@ def keepLinesPysam(snp, dir, barcodes):
     return good_snp
 
 
-def keepLines(snp, dir, barcodes):
-    good_snp = []
-    snp_coords = list(snp.keys())
-    snp_coords = [snp_coords[565]]
-    for i in range(0, len(snp_coords)):
-        # if i % 5000 == 0:
-        #     print(i)
-        print(i)
-        coord = snp_coords[i]
-        # coord = snp[i]
-        output = []
-        for file in os.listdir(dir):
-            if file.endswith(".bam"):
-                this_output = subprocess.check_output(["samtools", "view", "-F", "4", "-q", "30", str(dir) + "/" + file, coord])
-                # this_output = subprocess.check_output(["samtools", "view", "-F", "4", str(dir) + "/" + file, coord])
-                output_lines = this_output.decode().split("\n")
-                filtered_output_lines = filterCellranger(output_lines, barcodes[file.split(".")[0]])
-                # print(file.split(".")[0])
-                # print(len(filtered_output_lines))
-                output.extend(filtered_output_lines)
-                # len_output_lines = len(output_lines) - 1  # -1 because the last one is empty string
-                # output.extend(output_lines[:-1])
-        # output = filterCIGAR(output)
-        # output = filterCellranger(output, barcodes)
-        if len(output) < 1:
-            print("SNP NOT FOUND")
-        else:
-            if not isHomo(output, snp_coords[i]):
-                good_snp.append(snp[snp_coords[i]])
-    return good_snp
+# def keepLines(snp, dir, barcodes):
+#     """
+#     Junk now but keeping it in case the code is useful later on
+#     """
+#     good_snp = []
+#     snp_coords = list(snp.keys())
+#     snp_coords = [snp_coords[565]]
+#     for i in range(0, len(snp_coords)):
+#         # if i % 5000 == 0:
+#         #     print(i)
+#         print(i)
+#         coord = snp_coords[i]
+#         # coord = snp[i]
+#         output = []
+#         for file in os.listdir(dir):
+#             if file.endswith(".bam"):
+#                 this_output = subprocess.check_output(["samtools", "view", "-F", "4", "-q", "30", str(dir) + "/" + file, coord])
+#                 # this_output = subprocess.check_output(["samtools", "view", "-F", "4", str(dir) + "/" + file, coord])
+#                 output_lines = this_output.decode().split("\n")
+#                 filtered_output_lines = filterCellranger(output_lines, barcodes[file.split(".")[0]])
+#                 # print(file.split(".")[0])
+#                 # print(len(filtered_output_lines))
+#                 output.extend(filtered_output_lines)
+#                 # len_output_lines = len(output_lines) - 1  # -1 because the last one is empty string
+#                 # output.extend(output_lines[:-1])
+#         # output = filterCIGAR(output)
+#         # output = filterCellranger(output, barcodes)
+#         if len(output) < 1:
+#             print("SNP NOT FOUND")
+#         else:
+#             if not isHomo(output, snp_coords[i]):
+#                 good_snp.append(snp[snp_coords[i]])
+#     return good_snp
 
 def readBarcodes(barcodes_dir):
+    """
+    Read barcodes that are kept (aka not filtered out) in bb
+    :param barcodes_dir: directory of barcodes that are kept in bb
+    :return barcodes: dictionary of barcodes that are not filtered out in bb. Key is sample and value is list of barcodes
+    """
     barcodes = {}  # key is sample and value is barcodes
     for file in os.listdir(barcodes_dir):
         # if file.endswith("b1.txt"):  # TODO all bams
@@ -167,7 +180,6 @@ def readBarcodes(barcodes_dir):
 def main():
     snp_file, dir, verbose, outputFile, barcodes = parseArgs()
     # snp = ["NC_036780.1:118274-118845", "NC_036780.1:166532-244697", "NC_036780.1:272743-279989", "NC_036780.1:332525-366704", "NC_027944.1:14412-15552"]
-    # snp = ["NC_036780.1:272743-279989"]
     if verbose: print("Reading SNPs")
     snp = readSNP(snp_file)
     if verbose: print("Reading Barcodes")
