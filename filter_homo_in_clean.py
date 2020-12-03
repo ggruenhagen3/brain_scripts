5513daaec1926d30a5b00a8a2688e9687a63757d
George Gruenhagen <gwg@gatech.edu>
2020-12-03 14:46:00 -0500

    Filter out SNPs that are homo in clean cellranger reads

--- filter_homo_in_clean.py
+++ filter_homo_in_clean.py
@@ -47,7 +47,7 @@ def readSNP(snp_file):
 def filterCellrangerRead(line, barcodes):
     lineSplit = line.split("\t")
     info = lineSplit[11]
-    print(info)
+    # print(info)
     if "('xf', 25)" in info and "'CB'" in info and "'GN'" in info and lineSplit[4] == "255":
         barcode = line.split("'CB'")[1].split("'")[1]
         genes = line.split("'GN'")[1].split(")")[0]
