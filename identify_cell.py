a53a29d9d2331a7b37a9f9f9c190eaacc7f17cb1
ggruenhagen3 <ggruenhagen3@gatech.edu>
2021-01-11 15:15:18 -0500

    Debugging

--- identify_cell.py
+++ identify_cell.py
@@ -103,8 +103,9 @@ def identifyCell(dir, snp, valid_barcodes, verbose):
         for sample_snp in sample_snps:
             if verbose:
                 cur_snp_i += 1
-                if cur_snp_i % (len(sample_snps)/20) == 0:
-                    print(str(cur_snp_i / (len(sample_snps)/20)) + "/20 of " + snp_sample + " SNPs done")
+                print(cur_snp_i)
+                # if cur_snp_i % (len(sample_snps)/20) == 0:
+                #     print(str(cur_snp_i / (len(sample_snps)/20)) + "/20 of " + snp_sample + " SNPs done")
             scaffold = sample_snp.split(":")[0]
             pos = int(sample_snp.split(":")[1].split("-")[0])
             alt = sample_snp.split(":")[1].split("-")[1]
