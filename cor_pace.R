2d8f31b25d0042e3a50a1b8628137d2b6a7453e7
ggruenhagen3 <ggruenhagen3@gatech.edu>
2021-02-10 15:29:20 -0500

    Correlation Fast

--- cor_pace.R
+++ cor_pace.R
@@ -2,16 +2,14 @@ library("Seurat")
 library("Matrix")
 library("qvalue")
 library("jaccard")
-library("foreach")
-library("doParallel")
+library("parallel")
 print(paste("Num Cors:", detectCores()))
-lncRNA <- readRDS("/nv/hp10/ggruenhagen3/scratch/brain/data/lncRNA.RDS")
-obj <- lncRNA
-gene_names <- rownames(obj)[which(rowSums(as.matrix(obj@assays$RNA@counts)) > 0.001*ncol(obj))]
+bb <- readRDS("~/scratch/brain/data/bb_clustered_102820.rds")
+obj <- bb
+gene_names <- rownames(obj)[which(rowSums(as.matrix(obj@assays$RNA@counts)) > 2)]
 mat_data_p   = matrix(0, nrow=length(gene_names), ncol = length(gene_names), dimnames = list(gene_names, gene_names))
 mat_data_cor = matrix(0, nrow=length(gene_names), ncol = length(gene_names), dimnames = list(gene_names, gene_names))
 # test = as.matrix(t(obj@assays$RNA@data))
-test = obj@assays$RNA@data
 # ptm <- proc.time()
 # cl <- makeCluster(40, outfile="/nv/hp10/ggruenhagen3/scratch/brain/brain_scripts/cor_pace.log")
 # registerDoParallel(cl)
@@ -52,21 +50,43 @@ test = obj@assays$RNA@data
 # saveRDS(df_cor, "/nv/hp10/ggruenhagen3/scratch/brain/data/df_cor.RDS")
 # print("Saved correlations")
 
-for (col in 2:length(gene_names)) {
-  if (col %% 100 == 0) {
-    print(col)
-  }
-  gene1 = gene_names[col]
-  for ( row in 1:(col-1) ) {
-    gene2 = gene_names[row]
-    cor_res = cor.test(obj@assays$RNA@data[gene1,], obj@assays$RNA@data[gene2,])
-    # cor_res = cor.test(formula = ~ gene1 + gene2, data=test))
-    mat_data_p[row,col]   = cor_res$p.value
-    mat_data_cor[row,col] = cor_res$estimate
-  }
-}
+# for (col in 2:length(gene_names)) {
+#   if (col %% 100 == 0) {
+#     print(col)
+#   }
+#   gene1 = gene_names[col]
+#   for ( row in 1:(col-1) ) {
+#     gene2 = gene_names[row]
+#     cor_res = cor.test(obj@assays$RNA@data[gene1,], obj@assays$RNA@data[gene2,])
+#     # cor_res = cor.test(formula = ~ gene1 + gene2, data=test))
+#     mat_data_p[row,col]   = cor_res$p.value
+#     mat_data_cor[row,col] = cor_res$estimate
+#   }
+# }
 
 # system.time(cor.pvalues <- future_sapply(2:100, function(x) { sapply(1:(x-1), function(y) { cor.test(test[gene_names[x],],test[gene_names[y],])$p.value })}))
 
-saveRDS(mat_data_p,   "/nv/hp10/ggruenhagen3/scratch/brain/data/mat_data_p.RDS")
-saveRDS(mat_data_cor, "/nv/hp10/ggruenhagen3/scratch/brain/data/mat_data_cor.RDS")
+# Attempt as of 02/10/2021
+my_cor_t = function(r, n) (r * sqrt(n - 2))/sqrt(1 - r**2)
+my_cor_p = function(t, n) 2*pt(-abs(t), df=n-2)
+fx = function(x) cor(t(as.matrix(obj@assays$RNA@data[x,])), y = NULL)
+
+r_mat = cor(t(as.matrix(obj@assays$RNA@data[,])), y = NULL)
+t_mat = my_cor_t(r_mat, ncol(obj))
+p_mat = my_cor_p(t_mat, ncol(obj))
+
+saveRDS(r_mat, "~/scratch/brain/data/mat_data_cor.RDS")
+saveRDS(p_mat,   "~/scratch/brain/data/mat_data_cor_p.RDS")
+
+# gene_names <- rownames(bb)[which(rowSums(bb@assays$RNA@counts) > 2)]
+# fx <- function(gene1) cor.test(bb@assays$RNA@data[gene1, ], bb@assays$RNA@data["egr1", ])$p.value
+# print("No Parallel:")
+# system.time(
+#   results <- lapply(gene_names[1:200], fx)
+# )
+# 
+# print("Yes Parallel:")
+# numCores <- detectCores()
+# system.time(
+#   results <- mclapply(gene_names, fx, mc.cores = numCores)
+# )
