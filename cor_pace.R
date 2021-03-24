9abffbb42594f5d0b7b439cbb2f6b2b3f4aa8d95
ggruenhagen3 <ggruenhagen3@gatech.edu>
2021-03-24 11:11:58 -0400

    Paul Epi CTRL

--- cor_pace.R
+++ cor_pace.R
@@ -5,8 +5,10 @@ library("jaccard")
 library("parallel")
 print(paste("Num Cors:", detectCores()))
 # bb <- readRDS("~/scratch/brain/data/bb_clustered_102820.rds")
-incsr = readRDS("~/scratch/d_tooth/data/igor_incsr.rds")
-obj <- incsr
+paul_epi = readRDS("~/scratch/d_tooth/data/epi_full.rds")
+paul_epi$cond[is.na(paul_epi$cond)] <- "INJR"
+paul_ctrl = subset(paul_epi, cells = colnames(paul_epi)[which(paul_epi$cond == "CTRL")])
+obj <- paul_ctrl
 gene_names <- rownames(obj)[which(rowSums(as.matrix(obj@assays$RNA@counts)) > 2)]
 mat_data_p   = matrix(0, nrow=length(gene_names), ncol = length(gene_names), dimnames = list(gene_names, gene_names))
 mat_data_cor = matrix(0, nrow=length(gene_names), ncol = length(gene_names), dimnames = list(gene_names, gene_names))
@@ -76,8 +78,8 @@ r_mat = cor(t(as.matrix(obj@assays$RNA@data[,])), y = NULL)
 t_mat = my_cor_t(r_mat, ncol(obj))
 p_mat = my_cor_p(t_mat, ncol(obj))
 
-saveRDS(r_mat, "~/scratch/d_tooth/data/incsr_data_cor.RDS")
-saveRDS(p_mat,   "~/scratch/d_tooth/data/incsr_data_cor_p.RDS")
+saveRDS(r_mat, "~/scratch/d_tooth/data/paul_epi_ctrl_cor.RDS")
+saveRDS(p_mat,   "~/scratch/d_tooth/data/paul_epi_ctrl_cor_p.RDS")
 
 # gene_names <- rownames(bb)[which(rowSums(bb@assays$RNA@counts) > 2)]
 # fx <- function(gene1) cor.test(bb@assays$RNA@data[gene1, ], bb@assays$RNA@data["egr1", ])$p.value
