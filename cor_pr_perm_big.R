dfd45a37527af2e658e73ffa7fc1fe2591e36129
ggruenhagen3 <ggruenhagen3@gatech.edu>
2021-05-11 15:43:32 -0400

    10000 perms

--- cor_pr_perm_big.R
+++ cor_pr_perm_big.R
@@ -5,8 +5,8 @@
 
 # Constants
 output_folder = "~/scratch/brain/results/cor_pr_perm/"
-n_perm = 1000 # 200 matrices total because bhve and ctrl
-n_perm_per_job = 100
+n_perm = 10000 # 200 matrices total because bhve and ctrl
+n_perm_per_job = 500
 pbs_file_name = "~/scratch/brain/brain_scripts/cor_pr_perm.pbs"
 
 # Generate pbs script and submit jobs
@@ -15,7 +15,7 @@ sapply(1:(n_perm/n_perm_per_job), function(x) {
   fileConn <- file(pbs_file_name)
   writeLines(c("#PBS -A GT-js585",
                paste0("#PBS -N cor_pr_perm_", x),
-               "#PBS -l mem=400gb",
+               "#PBS -l mem=128gb",
                "#PBS -l nodes=2:ppn=4",
                "#PBS -l walltime=90:00:00",
                "#PBS -j oe",
