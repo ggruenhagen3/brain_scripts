66764fef12444bbc379b84c2efd46f7c0f75385e
ggruenhagen3 <ggruenhagen3@gatech.edu>
2021-05-27 15:29:19 -0400

    Predict behave perm

--- predict_behave_perm.py
+++ predict_behave_perm.py
@@ -34,8 +34,8 @@ def permSingleRun(i):
     ytest = numpy.multiply(["b" in x for x in list(xtest.index)], 1)
 
     # Find Genes with the largest cummulative difference between bhve and ctrl
-    bhve_names = [x for x in list(pd_df.index) if "b" in x]
-    ctrl_names = [x for x in list(pd_df.index) if "c" in x]
+    bhve_names = [x for x in list(xtrain.index) if "b" in x]
+    ctrl_names = [x for x in list(xtrain.index) if "c" in x]
     b_sum = xtrain.loc[xtrain.index.intersection(bhve_names)].sum(axis=0)
     c_sum = xtrain.loc[xtrain.index.intersection(ctrl_names)].sum(axis=0)
     dif = abs(b_sum - c_sum)
@@ -60,10 +60,50 @@ def permSingleRun(i):
     return(test_score)
 
 
+def pilotSingleRun():
+    # Set Constants for Number of Features to Select
+    num_cum_dif = 117
+    num_top_model = 4
+
+    # Read Data and Separate Train from Test
+    pd_df = pandas.read_csv("/storage/home/hcoda1/6/ggruenhagen3/scratch/brain/data/pilot_b1b2c1_avg_counts.csv", index_col=0)
+    train_names = [x for x in list(pd_df.index) if x != "b1" and x != "b2" and x != "c1"]  # the subsamples that are in the test data
+    test_names = [x for x in list(pd_df.index) if x == "b1" or x == "b2" or x == "c1"]  # the subsamples that are in the test data
+    xtrain = pd_df.loc[pd_df.index.intersection(train_names)]
+    xtest = pd_df.loc[pd_df.index.intersection(test_names)]
+    ytrain = numpy.multiply(["b" in x for x in list(xtrain.index)], 1)
+    ytest = numpy.multiply(["b" in x for x in list(xtest.index)], 1)
+
+    # Find Genes with the largest cummulative difference between bhve and ctrl
+    bhve_names = [x for x in list(xtrain.index) if "b" in x]
+    ctrl_names = [x for x in list(xtrain.index) if "c" in x]
+    b_sum = xtrain.loc[xtrain.index.intersection(bhve_names)].sum(axis=0)
+    c_sum = xtrain.loc[xtrain.index.intersection(ctrl_names)].sum(axis=0)
+    dif = abs(b_sum - c_sum)
+    dif_idx = (-dif).argsort()
+    xtrain = xtrain.iloc[:, dif_idx[0:num_cum_dif]]
+    xtest = xtest.iloc[:, dif_idx[0:num_cum_dif]]
+
+    # Select the Top Features to the Model
+    rc = LogisticRegression(C=1, solver='liblinear')
+    a = rc.fit(xtrain, ytrain)
+    importance = numpy.abs(rc.coef_)
+    sort_idx = (-importance).argsort()  # argsort give the indexes which would sort the array
+    xtrain = xtrain.iloc[:, sort_idx[0, 0:num_top_model]]
+    xtest = xtest.iloc[:, sort_idx[0, 0:num_top_model]]
+
+    print(ytest)
+    print(len(ytest))
+    # Refit the Model and Predict
+    a = rc.fit(xtrain, ytrain)
+    test_score = rc.score(xtest, ytest)
+
+    return (test_score)
+
 # 100 Permutations
 with multiprocessing.Pool(multiprocessing.cpu_count()) as pool:
     pool_res = pool.map(permSingleRun, range(1, 5))
     perm_df = pandas.DataFrame(pool_res)
     print(perm_df)
 
-
+# pd_df = pandas.read_csv("/storage/home/hcoda1/6/ggruenhagen3/scratch/brain/data/bb_subsample_counts.txt",sep="\s")
