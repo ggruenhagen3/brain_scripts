52bc3ca2a53c30392fb6ab27c89fbedd2a69841c
ggruenhagen3 <ggruenhagen3@gatech.edu>
2021-05-28 16:36:50 -0400

    Subset by gene option. Absolute value option

--- grn.py
+++ grn.py
@@ -196,8 +196,15 @@ def main():
                 ns_dict["B"] = pool_ns[0]
                 ns_dict["C"] = pool_ns[1]
                 ns_dict["Dif"] = pool_ns[0] - pool_ns[1]
+                ns_dict["Dif_Abs"] = np.absolute(pool_ns[0] - pool_ns[1])
             else:
                 ns_dict[i] = pool_ns[0] - pool_ns[1]
+            num_cells_bhve = np.array(data_mat[:, mat_idx['B' + str(i)]].astype(bool).sum(axis=1))[:, 0]
+            num_cells_ctrl = np.array(data_mat[:, mat_idx['C' + str(i)]].astype(bool).sum(axis=1))[:, 0]
+            gene_idx_min_cells = np.where(num_cells_bhve >= 5 and num_cells_ctrl >= 5)
+            print("Sums for genes with at least 5 cells in BHVE and CTRL")
+            print("Sum of NS Dif " + np.sum(pool_ns[0][gene_idx_min_cells] - pool_ns[1][gene_idx_min_cells]) )
+            print("Sum of Abs NS Dif " + np.sum(np.absolute(pool_ns[0][gene_idx_min_cells] - pool_ns[1][gene_idx_min_cells])))
         print(f"Done Permuting. Current Elapsed Time: {time.perf_counter() - start_time:0.4f} seconds")
     perm_ns_dif = pandas.DataFrame.from_dict(ns_dict,orient='index').transpose()
     perm_ns_dif.to_csv(output_folder + "/" + base_name + "_" + str(perm_num) + ".csv")
