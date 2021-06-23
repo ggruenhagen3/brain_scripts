fdf03538d427311ee71d3b2b01402fe283bedc47
ggruenhagen3 <ggruenhagen3@gatech.edu>
2021-06-23 14:32:17 -0400

    GRN IEG 5 Analsysi

--- cor_pr_perm_big.R
+++ cor_pr_perm_big.R
@@ -4,22 +4,22 @@
 #=============================================================+
 
 # Constants
-output_folder = "~/scratch/brain/results/cor_pr_perm/"
+output_folder = "~/scratch/brain/results/py_ns_ieg5/"
 n_perm = 10000 # 200 matrices total because bhve and ctrl
 n_perm_per_job = 500
-pbs_file_name = "~/scratch/brain/brain_scripts/cor_pr_perm.pbs"
+pbs_file_name = "~/scratch/brain/brain_scripts/py_ns_ieg5.pbs"
 
 # Generate pbs script and submit jobs
 system(paste0("mkdir -p ", output_folder))
 sapply(1:(n_perm/n_perm_per_job), function(x) {
   fileConn <- file(pbs_file_name)
   writeLines(c("#PBS -A GT-js585-biocluster",
-               paste0("#PBS -N cor_pr_perm_", x),
+               paste0("#PBS -N grn_ieg5_", x),
                "#PBS -l mem=128gb",
                "#PBS -l nodes=2:ppn=4",
                "#PBS -l walltime=90:00:00",
                "#PBS -j oe",
-               paste0("#PBS -o cor_pr_perm_", x, ".out"),
+               paste0("#PBS -o grn_ieg5_", x, ".out"),
                "#PBS -m abe",
                "#PBS -M gwg@gatech.edu\n",
                "cd $PBS_O_WORKDIR",
@@ -27,7 +27,7 @@ sapply(1:(n_perm/n_perm_per_job), function(x) {
                "module load anaconda3",
                "module load r",
                "conda activate r4\n",
-               paste("python grn.py", x, n_perm_per_job, "-a")),
+               paste("python grn.py", x, n_perm_per_job, "-a -i 5")),
              fileConn)
   close(fileConn)
   system(paste("qsub", pbs_file_name))
