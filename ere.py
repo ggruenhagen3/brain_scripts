9d91553f0e6986354806dbadf3432db7fd8b801a
ggruenhagen3 <ggruenhagen3@gatech.edu>
2021-06-29 14:14:19 -0400

    Find EREs

--- ere.py
+++ ere.py
@@ -8,24 +8,11 @@ def parseArgs():
     parser.add_argument("-i", "--input", help="Input fasta file", nargs="?",
                         default="/storage/home/hcoda1/6/ggruenhagen3/scratch/msc/GCF_000238955.4_M_zebra_UMD2a_genomic.fna",
                         const="/storage/home/hcoda1/6/ggruenhagen3/scratch/msc/GCF_000238955.4_M_zebra_UMD2a_genomic.fna")
+    parser.add_argument("-o", "--output", help="Output file in BED format", nargs="?",
+                        default="/storage/home/hcoda1/6/ggruenhagen3/scratch/msc/UMD2a_ERE.bed",
+                        const="/storage/home/hcoda1/6/ggruenhagen3/scratch/msc/UMD2a_ERE.bed")
     args = parser.parse_args()
-    return args.input
-
-# def detectMotif(tenth_split = None):
-#     ere_starts = []
-#     motif = "AGGTCA...TGACCT"
-#     prog = re.compile(motif)
-#     if tenth_split == None:
-#         res = prog.finditer(str(seq))
-#     else:
-#         tenth_length = len(seq) / 10
-#         res = prog.finditer(str(seq[int(tenth_length*tenth_split):int(tenth_length*tenth_split + tenth_length)]))
-#     for match in res:
-#         match_start = match.span()[0]
-#         if tenth_split != None:
-#             match_start = match_start + tenth_length
-#         ere_starts.append(match_start)
-#     return ere_starts
+    return args.input, args.output
 
 def detectMotifInRecord(record):
     ere_starts = []
@@ -40,12 +27,15 @@ def detectMotifInRecord(record):
 def writeFile(file, lines):
     f = open(file, "w")
     for line in lines:
-        f.write(line + "\n")
+        chr = line.split(":")[0]
+        start = line.split(":")[1].split("-")[0]
+        stop = line.split(":")[1].split("-")[1]
+        f.write(chr + "\t" + start + "\t" + stop + "\n")
     f.close()
 
 
 def main():
-    input = parseArgs()
+    input, output = parseArgs()
     print("Reading input file: " + input)
 
     records = list(SeqIO.parse(input, "fasta"))
@@ -54,7 +44,10 @@ def main():
     with multiprocessing.Pool(multiprocessing.cpu_count()) as pool:
         all_eres.extend(pool.map(detectMotifInRecord, records))
     all_eres = [item for sublist in all_eres for item in sublist]  # flatten all_eres list
-    writeFile("/storage/home/hcoda1/6/ggruenhagen3/scratch/msc/UMD2a_ERE.txt", all_eres)
+
+    # Printing output
+    print("Writing output in BED format to: " + output)
+    writeFile(output, all_eres)
 
 
 if __name__ == '__main__':
