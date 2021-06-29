137e498dab9425ac301a777ce0e9cc6aa93052c3
ggruenhagen3 <ggruenhagen3@gatech.edu>
2021-06-29 14:35:10 -0400

    Find EREs

--- ere.py
+++ ere.py
@@ -11,8 +11,9 @@ def parseArgs():
     parser.add_argument("-o", "--output", help="Output file in BED format", nargs="?",
                         default="/storage/home/hcoda1/6/ggruenhagen3/scratch/msc/UMD2a_ERE.bed",
                         const="/storage/home/hcoda1/6/ggruenhagen3/scratch/msc/UMD2a_ERE.bed")
+    parser.add_argument("-v", "--vcf", help="Make output vcf-like?", action="store_true")
     args = parser.parse_args()
-    return args.input, args.output
+    return args.input, args.output, args.vcf
 
 def detectMotifInRecord(record):
     ere_starts = []
@@ -24,18 +25,21 @@ def detectMotifInRecord(record):
         ere_starts.append(record.id + ":" + str(match.span()[0]) + "-" + str(match.span()[1]))
     return ere_starts
 
-def writeFile(file, lines):
+def writeFile(file, lines, vcf):
     f = open(file, "w")
     for line in lines:
         chr = line.split(":")[0]
         start = line.split(":")[1].split("-")[0]
         stop = line.split(":")[1].split("-")[1]
-        f.write(chr + "\t" + start + "\t" + stop + "\n")
+        if vcf:
+            f.write(chr + "\t" + start + "\t" + stop + ".\tA\tA\t1.0\t1\n")
+        else:
+            f.write(chr + "\t" + start + "\t" + stop + "\n")
     f.close()
 
 
 def main():
-    input, output = parseArgs()
+    input, output,vcf = parseArgs()
     print("Reading input file: " + input)
 
     records = list(SeqIO.parse(input, "fasta"))
@@ -45,9 +49,12 @@ def main():
         all_eres.extend(pool.map(detectMotifInRecord, records))
     all_eres = [item for sublist in all_eres for item in sublist]  # flatten all_eres list
 
+    if vcf:
+        output.replace(".bed", ".vcf")
+
     # Printing output
     print("Writing output in BED format to: " + output)
-    writeFile(output, all_eres)
+    writeFile(output, all_eres, vcf)
 
 
 if __name__ == '__main__':
