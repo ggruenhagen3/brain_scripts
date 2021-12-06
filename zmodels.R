c377fc1529bde983cabfe04e701cd5f2541555ad
ggruenhagen3 <ggruenhagen3@gatech.edu>
2021-12-06 11:12:29 -0500

    Speeding up Zack's Models

--- zmodels.R
+++ zmodels.R
@@ -17,10 +17,11 @@ k = 0
 
 df = bb@meta.data[which(bb$seuratclusters15 == k),]
 df$pair = as.factor(df$pair)
+df$subject = as.factor(df$trial_id)
 
-bbmm_start_time <- proc.time()
+bbmm_start_time <- proc.time()[[3]]
 bbmm <- BBmm(fixed.formula = neurogen_score ~ as.numeric(bower_activity_index) + as.numeric(gsi), random.formula = ~ (subject %in% sample %in% run) + (subject %in% pair) , m=88, data = df, show = TRUE)
-bbmm_stop_time = proc.time()
+bbmm_stop_time = proc.time()[[3]]
 print(paste0("BBmm on Cluster 0 (15 level) took: ", bbmm_stop_time-bbmm_start_time))
 
 saveRDS(bbmm, "~/scratch/brain/results/bbmm_test.rds")
