19707949d8c4635d5f13b5f3314d7652ec953ad3
George <ggruenhagen3@gatech.edu>
2021-12-07 16:04:42 -0500

    Testing

--- zmodels.R
+++ zmodels.R
@@ -85,11 +85,11 @@ num.cores = detectCores()
 print(paste0("Number of Cores: ", num.cores))
 print(paste0("BBmm Start Time: ", format(Sys.time(), "%X")))
 bbmm_start_time <- proc.time()[[3]]
-# res2 = myBBmmVector("neurogen_score")
+res2 = myBBmmVector("neurogen_score")
 # res = unlist(mclapply(run_vars, function(x) myBBmm(x), mc.cores = num.cores))
 # names(res) = run_vars
 # print(res)
-bbmm <- BBmm(fixed.formula = neurogen_score ~ as.numeric(bower_activity_index) + as.numeric(gsi), random.formula = ~ (subject %in% sample %in% run) + (subject %in% pair) , m=88, data = df, show = TRUE)
+# bbmm <- BBmm(fixed.formula = neurogen_score ~ as.numeric(bower_activity_index) + as.numeric(gsi), random.formula = ~ (subject %in% sample %in% run) + (subject %in% pair) , m=88, data = df, show = TRUE)
 bbmm_stop_time = proc.time()[[3]]
 print(paste0("BBmm on Cluster 0 (15 level) on Real took: ", bbmm_stop_time-bbmm_start_time))
 print(paste0("BBmm End Time: ", format(Sys.time(), "%X")))
