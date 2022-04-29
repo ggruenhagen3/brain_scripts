f174571d1ca2d764b5926603e307c4f0c93dbb34
ggruenhagen3 <ggruenhagen3@gatech.edu>
2022-04-29 15:46:07 -0400

    mod_z

--- pcrc_neurogen_perm.R
+++ pcrc_neurogen_perm.R
@@ -7,6 +7,16 @@ randomCombo = function(x) {
   random_neurogen = sample(not_neurogen_clust, n_neurogen_clust)
   return(mean(cor_mat[random_pcrc, random_neurogen]))
 }
+randomCombo2 = function(x) {
+  set.seed(x)
+  random_pcrc     = sample(pcrc,     n_pcrc_clust)
+  random_neurogen = sample(neurogen, n_neurogen_clust)
+  while( any(random_pcrc %in% pcrc_clust) && any(random_neurogen %in% neurogen_clust) )  {
+    random_pcrc     = sample(pcrc,     n_pcrc_clust)
+    random_neurogen = sample(neurogen, n_neurogen_clust)
+  }
+  return(mean(cor_mat[random_pcrc, random_neurogen]))
+}
 createPermDataMat = function(x) {
   set.seed(x)
   this_data_mat = do.call('rbind', lapply(1:nrow(data_mat), function(y) sample(data_mat[y,])))
@@ -68,8 +78,20 @@ cor_mat = cor(x = t(as.matrix(data_mat)))
 cor_mat = cor_mat[pcrc, neurogen]
 cor_mat[is.na(cor_mat)] = 0
 
-pcrc_clust     = c("cobl", "LOC101479283", "wdr73", "plekhg4b", "grik5", "LOC101476487", "LOC101476914", "ddr1", "LOC101477204", "plekhf2")
-neurogen_clust = c("csf1r", "LOC101480727", "vegfa", "LOC101484715", "arhgef10", "stat3", "erbb2", "smo", "epha3", "LOC101469419", "LOC101487687", "boc", "pax6", "metrn", "LOC101469466")
+# mod_me
+# pcrc_clust     = c("cobl", "LOC101479283", "wdr73", "plekhg4b", "grik5", "LOC101476487", "LOC101476914", "ddr1", "LOC101477204", "plekhf2")
+# neurogen_clust = c("csf1r", "LOC101480727", "vegfa", "LOC101484715", "arhgef10", "stat3", "erbb2", "smo", "epha3", "LOC101469419", "LOC101487687", "boc", "pax6", "metrn", "LOC101469466")
+
+# mod_z
+pcrc_clust     = c("LOC101479283", "cobl", "wdr73", "grik5", "iglon5", "LOC101477204")
+neurogen_clust = c("epha3", "LOC101480727", "neo1", "pax6", "boc", "LOC101465090", "vegfa", "LOC101487591", "LOC101482557", "LOC101464345", "metrn", "cyfip2", "LOC101466433", "numb", "LOC101487687", "LOC101469419", "LOC101478875", "tenm4")
+rgc_sub = readRDS("~/scratch/brain/data//rgc_subclusters_reclustered_q_c_nb_scores.rds")
+data_mat = rgc_sub@assays$RNA@data[c(pcrc, neurogen),]
+full_counts_mat = rgc_sub@assays$RNA@counts[c(pcrc, neurogen),]
+cor_mat = cor(x = t(as.matrix(data_mat)))
+cor_mat = cor_mat[pcrc, neurogen]
+cor_mat[is.na(cor_mat)] = 0
+
 n_pcrc_clust     = length(pcrc_clust)
 n_neurogen_clust = length(neurogen_clust)
 not_pcrc_clust     = rownames(cor_mat)[which(!rownames(cor_mat) %in% pcrc_clust)]
@@ -81,29 +103,30 @@ real_mean = mean(cor_mat[pcrc_clust, neurogen_clust])
 # have as strong of an average correlation coefficient?
 print(paste0("Starting Random Combos: ", format(Sys.time(), "%X ")))
 library(parallel)
-nperm = 10000
-random_means = unlist(mclapply(1:nperm, function(x) randomCombo(x), mc.cores = detectCores()))
+random_means = unlist(mclapply(1:1000000, function(x) randomCombo2(x), mc.cores = detectCores()))
 print(paste0("Number of Random Combo Average Correlation Greater Than Real: ", length(which(random_means >= real_mean))))
 print(paste0("Random Combos Done: ", format(Sys.time(), "%X ")))
+write.csv(random_means, "~/scratch/brain/results/pcrc_neurogen_z_random_combos2_1_million_042922.csv")
 
 # 2
 # Is the average correlation coefficient of the module greater than permutations of the module?
 # Where a permutation is when the expression values for each are shuffled separately.
 ## perm_data_mats = mclapply(1:nperm, function(x) createPermDataMat(x), mc.cores = detectCores())
-# print("")
-# print(paste0("Starting Permutations of the Module: ", format(Sys.time(), "%X ")))
-# perm_mod_means = unlist(mclapply(1:nperm, function(x) permModule(x), mc.cores = detectCores()))
-# print(paste0("Number of Permutations of the Module Greater Than Real: ", length(which(perm_mod_means >= real_mean))))
-# print(paste0("Permutations of the Module Done: ", format(Sys.time(), "%X ")))
-# 
-# print("")
-# print(paste0("Writing Output to csv: ", format(Sys.time(), "%X ")))
-# write.csv(data.frame(random_combo_mean = random_means, perm_mod_mean = perm_mod_means, real_mean = real_mean), "~/scratch/brain/results/pcrc_neurogen_perm_042822.csv")
-# print(paste0("All Done: ", format(Sys.time(), "%X ")))
+nperm = 10000
+print("")
+print(paste0("Starting Permutations of the Module: ", format(Sys.time(), "%X ")))
+perm_mod_means = unlist(mclapply(1:nperm, function(x) permModule(x), mc.cores = detectCores()))
+print(paste0("Number of Permutations of the Module Greater Than Real: ", length(which(perm_mod_means >= real_mean))))
+print(paste0("Permutations of the Module Done: ", format(Sys.time(), "%X ")))
+
+print("")
+print(paste0("Writing Output to csv: ", format(Sys.time(), "%X ")))
+write.csv(data.frame(perm_mod_mean = perm_mod_means, real_mean = real_mean), "~/scratch/brain/results/pcrc_neurogen_z_perm_042922.csv")
+print(paste0("All Done: ", format(Sys.time(), "%X ")))
 
 print("")
 print(paste0("Loading RGC Object: ", format(Sys.time(), "%X ")))
-rgc_sub = readRDS("~/scratch/brain/data//rgc_subclusters_reclustered_q_c_nb_scores.rds")
+rgc_sub = readRDS("~/scratch/brain/data/rgc_subclusters_reclustered_q_c_nb_scores.rds")
 
 # Module Relationship to Quiescent and Cycling RGCs
 # 3a. Is the correlation between module score and quiescent score in RGCs greater than permutations?
@@ -137,5 +160,5 @@ print(paste0("Number of Permutations of the Module Stronger Than Real - Mod Quie
 print(paste0("Number of Permutations of the Module Stronger Than Real - Mod Quiescent: ", length(which(perm_mod_rgc_df$perm_mod_neurogen_qui >= real_mod_neurogen_qui))))
 print(paste0("Number of Permutations of the Module Stronger Than Real - Mod Quiescent: ", length(which(perm_mod_rgc_df$perm_mod_neurogen_cyc <= real_mod_neurogen_cyc))))
 
-write.csv(perm_mod_rgc_df, "~/scratch/brain/results/pcrc_neurogen_perm_mod_rgc_042822.csv")
+write.csv(perm_mod_rgc_df, "~/scratch/brain/results/pcrc_neurogen_z_perm_mod_rgc_042822.csv")
 
