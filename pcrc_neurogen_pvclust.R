ce8fe9dd78f4818185ef7c2cb5da94043e6a136b
ggruenhagen3 <ggruenhagen3@gatech.edu>
2022-05-05 12:47:02 -0400

    1000 permutations

--- pcrc_neurogen_pvclust.R
+++ pcrc_neurogen_pvclust.R
@@ -10,6 +10,8 @@ nperm = 10000
 print(paste0("Loading Data: ", format(Sys.time(), "%X ")))
 neurogen = read.csv("~/scratch/brain/data/conserved_neurogenesis_positive88_zfish_mouse_cichlid.csv")[,3]
 pcrc = read.csv("~/scratch/brain/fst/pc_20_rc_20_10kb_bins_25kb_genes_on_lg_11_peak_by_bin.csv")[,2]
+pcrc_neurogen_rowSums = rowSums(bb@assays$RNA@counts[c(pcrc, neurogen),] > 0)
+data_mat_c = t(bb@assays$RNA@data[names(pcrc_neurogen_rowSums)[which(pcrc_neurogen_rowSums > 0)],])
 
 cl <- makeCluster(24, type = "PSOCK")
 fit = pvclust::parPvclust(cl=cl, data = as.matrix(data_mat_c), method.hclust = "average", method.dist = "correlation", nboot = 1000)
