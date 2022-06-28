4a999a08e0640921119be17bfd4f4708b1510deb
ggruenhagen3 <ggruenhagen3@gatech.edu>
2022-06-28 09:04:50 -0400

    Spatial

---
+++ spatial.R
@@ -0,0 +1,4 @@
+
+dir_of_sr_dirs = "C:/Users/miles/Downloads/sp_data/"
+v293c1 = Load10X_Spatial(paste0(dir_of_sr_dirs, "/JTS12_293_C1/outs/"))
+v295a1 = Load10X_Spatial(paste0(dir_of_sr_dirs, "/JTS12_295_A1/outs/"))
