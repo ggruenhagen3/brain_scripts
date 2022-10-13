4dd945ddee5bff9db1b4981f4c41c303fee491ec
ggruenhagen3 <ggruenhagen3@gatech.edu>
2022-10-13 11:14:28 -0400

    Multiple Resolutions in 1 run

--- cluster_boot.R
+++ cluster_boot.R
@@ -4,12 +4,11 @@ args = commandArgs(trailingOnly=TRUE)
 boot_num = as.numeric(args[1])
 min_dist = as.numeric(args[2])
 n_neighbors = as.numeric(args[3])
-res   = as.numeric(args[4])
-nboot = as.numeric(args[5])
+nboot = as.numeric(args[4])
 num_cores = 10
-if (length(args) > 5) { num_cores = as.numeric(args[6])}
+if (length(args) > 5) { num_cores = as.numeric(args[5])}
 boot.pct = 0.8
-message(paste0("Running w/ Parameters: boot_num=", boot_num, ", min_dist=", min_dist, ", n_neighbors=", n_neighbors, ", res=", res, ", nboot=", nboot, ", num_cores=", num_cores))
+message(paste0("Running w/ Parameters: boot_num=", boot_num, ", min_dist=", min_dist, ", n_neighbors=", n_neighbors, ", nboot=", nboot, ", num_cores=", num_cores))
 
 # Load Libraries ===============================================================
 suppressMessages(library('parallel',  quietly = T, warn.conflicts = F, verbose = F))
@@ -24,6 +23,7 @@ orig.labels = all.orig.labels[,paste0("mindist", min_dist, "_nneighbors", n_neig
 
 # Helper Functions =============================================================
 sc.cluster.boot = function(x) {
+  message(paste0("Starting Boot=", x))
   message("Subsampling")
   this.cells = sample(colnames(obj@assays$RNA@counts), num.boot.cells, replace = F)
   this.obj = CreateSeuratObject(counts = obj@assays$RNA@counts[,this.cells], meta.data = obj@meta.data[this.cells,])
@@ -59,21 +59,33 @@ evaluate.boot = function(x) {
 # boot.labels is a data.frame with rows corresponding to cells and columns 
 # corresponding to bootstraps. The value of each element is the cluster the 
 # cell was assigned to in that bootstap.
-boot.labels = as.data.frame(mclapply(1:nboot, function(x) sc.cluster.boot(x), mc.cores = num_cores))
-colnames(boot.labels) = 1:nboot
-write.csv(boot.labels, paste0("~/scratch/brain/results/hb/cluster_boot/boots/mindist", min_dist, "_nneighbors", n_neighbors, "_res", res, "_labels.csv"))
-
-# cluster.jaccard is a data.frame with rows corresponding to clusters in the original
-# clustering. While the columns are the bootstraps. Each element is the highest jaccard index
-# of the original cluster in any of the bootstrap clusters from that bootstrap.
-cluster.jaccard.list = mclapply(1:nboot, function(x) evaluate.boot(x), mc.cores = num_cores)
-cluster.jaccard = do.call('cbind', cluster.jaccard.list)
-write.csv(cluster.jaccard, paste0("~/scratch/brain/results/hb/cluster_boot/boots/mindist", min_dist, "_nneighbors", n_neighbors, "_res", res, "_jcrd.csv"))
-
-# Visualize the Results
-cluster.jaccard.melt = reshape2::melt(cluster.jaccard)
-cluster.jaccard.melt[,1] = cluster.jaccard.melt[,1] - 1
-colnames(cluster.jaccard.melt) = c("cluster", "boot", "value") # TODO is this correct name order
-pdf(paste0("~/scratch/brain/results/hb/cluster_boot/boots/mindist", min_dist, "_nneighbors", n_neighbors, "_res", res, "_jcrd.pdf"), width = 12, height = 6)
-print(ggplot(cluster.jaccard.melt, aes(x = cluster, y = value, color = cluster, fill = cluster, group = cluster)) + geom_point() + NoLegend())
-dev.off()
+resolutions = seq(0.2, 2, by = 0.2)
+for (res in resolutions) {
+  message(paste0(" - - - Starting Resolution=", res, " (George) - - - "))
+  start.time = proc.time()
+  
+  boot.labels = as.data.frame(mclapply(1:nboot, function(x) sc.cluster.boot(x), mc.cores = num_cores))
+  colnames(boot.labels) = 1:nboot
+  write.csv(boot.labels, paste0("~/scratch/brain/results/hb/cluster_boot/boots/mindist", min_dist, "_nneighbors", n_neighbors, "_res", res, "_labels.csv"))
+  
+  # cluster.jaccard is a data.frame with rows corresponding to clusters in the original
+  # clustering. While the columns are the bootstraps. Each element is the highest jaccard index
+  # of the original cluster in any of the bootstrap clusters from that bootstrap.
+  cluster.jaccard.list = mclapply(1:nboot, function(x) evaluate.boot(x), mc.cores = num_cores)
+  cluster.jaccard = do.call('cbind', cluster.jaccard.list)
+  write.csv(cluster.jaccard, paste0("~/scratch/brain/results/hb/cluster_boot/boots/mindist", min_dist, "_nneighbors", n_neighbors, "_res", res, "_jcrd.csv"))
+  
+  # Visualize the Results
+  cluster.jaccard.melt = reshape2::melt(cluster.jaccard)
+  cluster.jaccard.melt[,1] = cluster.jaccard.melt[,1] - 1
+  colnames(cluster.jaccard.melt) = c("cluster", "boot", "value") # TODO is this correct name order
+  pdf(paste0("~/scratch/brain/results/hb/cluster_boot/boots/mindist", min_dist, "_nneighbors", n_neighbors, "_res", res, "_jcrd.pdf"), width = 12, height = 6)
+  print(ggplot(cluster.jaccard.melt, aes(x = cluster, y = value, color = cluster, fill = cluster, group = cluster)) + geom_point() + NoLegend())
+  dev.off()
+  
+  end.time = proc.time()
+  elapsed.time = end.time - start.time
+  elapsed.time = round(elapsed.time["elapsed"])
+  message(paste0(" - - - Finished Resolution=", res, " in ", elapsed.time, "s (George) - - - "))
+}
+message("All Done")
