41bf77285e94fbbaed5c9c4256be596c44131940
ggruenhagen3 <ggruenhagen3@gatech.edu>
2022-10-26 11:36:32 -0400

    Analyze chooseR results

--- hb.R
+++ hb.R
@@ -18,8 +18,77 @@ setwd(out_dir)
 # hb = readRDS("~/research/brain/data/hb_mi")
 
 #*******************************************************************************
-# chooseR ======================================================================
+# Clustering Robustness ========================================================
 #*******************************************************************************
+source("/storage/home/hcoda1/6/ggruenhagen3/scratch/brain/chooser/my/cluster.stability/R/pipeline.R")
+batch_num = 1
+results_path = paste0("/storage/home/hcoda1/6/ggruenhagen3/scratch/brain/chooser/my/cluster.stability/examples/batch_data_", batch_num, "/")
+min_dists = c(0.001, 0.1, 0.2, 0.3, 0.4, 0.5)
+# min_dists = c(0.001, 0.1)
+n_neighbors = c(5, 10, 20, 30, 40, 50)
+resolutions = seq(0.2, 2.0, by = 0.2)
+scores = list()
+for (min_dist in min_dists) {
+  for (n_neighbor in n_neighbors) {
+    for (res in resolutions) {
+      this.str = paste0("X", batch_num, "_", min_dist, "_", n_neighbor, "_res.", res)
+      this.sil =  readRDS(paste0(results_path, "silhouette_grouped_", this.str, ".rds"))
+      this.sil$min_dist = min_dist
+      this.sil$n_neighbor = n_neighbor
+      scores[[length(scores)+1]] = this.sil
+    }
+  }
+}
+
+# scores <- purrr::map(
+#   paste0(results_path, "silhouette_grouped_", paste0("X", batch_num, "_", min_dists, "_", n_neighbors, "_res.", resolutions), ".rds"),
+#   readRDS
+# )
+
+library("dplyr")
+scores <- dplyr::bind_rows(scores) %>%
+  dplyr::group_by(min_dist, n_neighbor, res) %>%
+  dplyr::mutate("n_clusters" = dplyr::n()) %>%
+  dplyr::ungroup()
+
+unique(data.frame(scores)[,c("min_dist", "n_neighbor", "res", "n_clusters")])
+range(unique(data.frame(scores)[,c("min_dist", "n_neighbor", "res", "n_clusters")])$n_clusters)
+
+meds <- scores %>%
+  dplyr::group_by(min_dist, n_neighbor, res) %>%
+  dplyr::summarise(
+    "boot" = list(boot_median(avg_sil)),
+    "n_clusters" = mean(n_clusters)
+  ) %>%
+  tidyr::unnest_wider(boot)
+
+threshold <- max(meds$low_med)
+meds %>% dplyr::filter(med >= threshold) %>% dplyr::arrange(n_clusters) %>% tail(n = 1)
+meds$id = paste(meds$min_dist, meds$n_neighbor, meds$res, sep = "_")
+scores$id = paste(scores$min_dist, scores$n_neighbor, scores$res, sep = "_")
+meds = meds %>% dplyr::arrange(n_clusters)
+choice <- as.character(meds %>% dplyr::filter(med >= threshold) %>% dplyr::arrange(n_clusters) %>% tail(n = 1) %>% dplyr::pull(id))
+
+p.meds = data.frame(meds)
+p.meds$id = factor(p.meds$id, levels = unique(p.meds$id))
+ggplot(p.meds, aes(id, med)) + geom_crossbar(aes(ymin = low_med, ymax = high_med), fill = "grey", size = 0.25) +
+                               geom_hline(aes(yintercept = threshold), colour = "blue") +
+                               geom_vline(aes(xintercept = choice), colour = "red") +
+                               scale_x_discrete("Min Distance _ N Neighbors _ Resolution") +
+                               scale_y_continuous("Silhouette Score", expand = c(0, 0), limits = c(-1, 1), breaks = seq(-1, 1, 0.25), oob = scales::squish) +
+                               cowplot::theme_minimal_hgrid() +
+                               theme(axis.title = element_text(size = 8), axis.text.y = element_text(size = 7), axis.text.x = element_text(size = 7, angle = 45, vjust = 1, hjust = 1), axis.line.x = element_line(colour = "black"), axis.line.y = element_line(colour = "black"), axis.ticks = element_line(colour = "black"),)
+ggsave(filename = paste0("~/scratch/brain/results/hb/test_sdp.png"), dpi = 300, height = 3.5, width = 7, units = "in")
+# ggplot(data.frame(meds), aes(id, med)) + geom_crossbar(aes(ymin = low_med, ymax = high_med), fill = "grey", size = 0.25) +
+#   geom_hline(aes(yintercept = threshold), colour = "blue") +
+#   geom_vline(aes(xintercept = choice), colour = "red") +
+#   geom_jitter(data = scores, aes(id, avg_sil), size = 0.35, width = 0.15) +
+#   scale_x_discrete("Min Distance _ N Neighbors _ Resolution") +
+#   scale_y_continuous("Silhouette Score", expand = c(0, 0), limits = c(-1, 1), breaks = seq(-1, 1, 0.25), oob = scales::squish) +
+#   cowplot::theme_minimal_hgrid() +
+#   theme(axis.title = element_text(size = 8), axis.text = element_text(size = 7, angle = 45, vjust = 1, hjust = 1), axis.line.x = element_line(colour = "black"), axis.line.y = element_line(colour = "black"), axis.ticks = element_line(colour = "black"),)
+
+# Real Run
 my.cluster = function(res) {
         this.obj = RunUMAP(this.obj, dims=1:50, min.dist=min_dist, spread=1, n.neighbors=n_neighbors, n.epochs=1000, metric="euclidean")
         this.obj = FindNeighbors(this.obj, reduction="umap", k.param=n_neighbors, dims=1:2, n.trees=500, prune.SNN=0)
@@ -44,6 +113,93 @@ for (min_dist in c(0.001, seq(0.1, 0.5, by = 0.1))) {
 all.clustering.df[,1] = NULL
 write.csv(all.clustering.df, "~/scratch/brain/results/hb/hb_clustering_options101222.csv")
 
+all.clustering.df = read.csv("~/scratch/brain/results/hb/hb_clustering_options101222.csv")
+all.clustering.df$X = NULL
+# all.rand = data.frame()
+# for (i in 1:(ncol(all.clustering.df)-1)) {
+#   print(paste0("i = ", i))
+#   for (j in (i+1):ncol(all.clustering.df)) {
+#     all.rand = rbind(all.rand, data.frame(i = i, j = j, name.i = colnames(all.clustering.df)[i], name.j = colnames(all.clustering.df)[j], rand = fossil::rand.index(all.clustering.df[,i], all.clustering.df[,j])))
+#   }
+# }
+rand_pair = function(this.str) {
+  i = as.numeric(strsplit(this.str, "_")[[1]][1])
+  j = as.numeric(strsplit(this.str, "_")[[1]][2])
+  this.rand = fossil::rand.index(all.clustering.df[,i], all.clustering.df[,j])
+  return(this.rand)
+}
+
+# strs.i.j = c()
+# for (i in 1:(ncol(all.clustering.df)-1)) { for (j in (i+1):ncol(all.clustering.df)) { strs.i.j = c(strs.i.j, paste0(i, "_", j)) } }
+strs.i.j = expand.grid(1:600, 1:600)
+strs.i.j <- data.table::setDT(data.table::CJ(strs.i.j[,1], strs.i.j[,2], unique = TRUE))
+strs.i.j = as.data.frame(strs.i.j[!duplicated(t(apply(strs.i.j, 1, sort))),])
+strs.i.j = strs.i.j[which(strs.i.j[,1] != strs.i.j[,2]),]
+strs.i.j = paste0(as.vector(strs.i.j[,1]), "_", as.vector(strs.i.j[,2]))
+# rand.list = unlist(parallel::mclapply(strs.i.j, function(x) rand_pair(x), mc.cores = 24))
+
+all.rand = data.frame()
+for (i in 1:10) {
+  this.rand = read.csv(paste0("~/scratch/brain/results/hb/cluster_boot/rand/rand_", i, ".csv"))
+  this.rand$X = NULL
+  this.rand$str = strs.i.j[this.rand$combo.idx]
+  all.rand = rbind(all.rand, this.rand)
+}
+all.rand[,c('i','j')] = reshape2::colsplit(all.rand$str, "_", c('1', '2'))
+rownames(all.rand) = all.rand$str
+
+all.n.clust = unlist(lapply(1:ncol(all.clustering.df), function(x) max(all.clustering.df[,x])+1 ))
+all.rand$i.n.clust = all.n.clust[all.rand$i]
+all.rand$j.n.clust = all.n.clust[all.rand$j]
+# pdf("~/scratch/brain/results/hb/test.pdf", width = 7, height = 7)
+all.rand = all.rand[order(all.rand$rand, decreasing = F),]
+Cairo::Cairo(file = "~/scratch/brain/results/hb/test.png", width = 2400, height = 2400, dpi = 200)
+print(ggplot(all.rand, aes(x = i.n.clust, y = j.n.clust, alpha = rand, color = rand)) + geom_point() + viridis::scale_color_viridis())
+dev.off()
+
+clust.max = unlist(lapply(1:ncol(all.clustering.df), function(x) max(all.clustering.df[,x])))
+small.bin.start = seq(10, 240, by = 5)
+small.bin.stop  = seq(15, 245, by = 5)-1
+big.bin.start = seq(0,  225, by = 25)
+big.bin.stop  = seq(25, 250, by = 25)-1
+small.bin.df = data.frame(start = small.bin.start, stop = small.bin.stop)
+small.bin.df$Freq = unlist(lapply(1:nrow(small.bin.df), function(x) length(which(clust.max >= small.bin.df$start[x] & clust.max <= small.bin.df$stop[x])) ))
+
+small.bin.rand = data.frame()
+for (i in 17:nrow(small.bin.df)) {
+  print(i)
+  bin.idx = which(clust.max >= small.bin.df$start[i] & clust.max <= small.bin.df$stop[i])
+  if (length(bin.idx) > 1) {
+    this.strs.i.j = expand.grid(bin.idx, bin.idx)
+    this.strs.i.j = data.table::setDT(data.table::CJ(this.strs.i.j[,1], this.strs.i.j[,2], unique = TRUE))
+    this.strs.i.j = as.data.frame(this.strs.i.j[!duplicated(t(apply(this.strs.i.j, 1, sort))),])
+    this.strs.i.j = this.strs.i.j[which(this.strs.i.j[,1] != this.strs.i.j[,2]),]
+    this.strs.i.j = paste0(as.vector(this.strs.i.j[,1]), "_", as.vector(this.strs.i.j[,2]))
+    
+    already.calc.strs = this.strs.i.j[which(this.strs.i.j %in% all.rand$str)]
+    if (i < 17) {
+      already.calc.df = data.frame(bin = i, str = already.calc.strs, rand = all.rand[already.calc.strs, "rand"])
+      not.calc.strs = this.strs.i.j[which(!this.strs.i.j %in% all.rand$str)]
+      not.calc.df = data.frame(bin = i, str = not.calc.strs, rand = NA)
+      this.rand = unlist(parallel::mclapply(not.calc.strs, function(x) rand_pair(x), mc.cores = 3))
+      not.calc.df$rand = this.rand
+      this.df = rbind(already.calc.df, not.calc.df)
+    } else {
+      not.calc.strs = this.strs.i.j
+      not.calc.df = data.frame(bin = i, str = not.calc.strs, rand = NA)
+      this.rand = unlist(parallel::mclapply(not.calc.strs, function(x) rand_pair(x), mc.cores = 3))
+      not.calc.df$rand = this.rand
+      this.df = not.calc.df
+    }
+    
+    small.bin.rand = rbind(small.bin.rand, this.df)
+  }
+}
+
+small.bin.rand$start = small.bin.df$start[small.bin.rand$bin]
+pdf("~/scratch/brain/results/hb/cluster_boot/rand/small_bin_rand.pdf", width = 10, height = 5)
+ggplot(small.bin.rand, aes(x = start, y = rand, color = start, fill = start, group = start)) + geom_boxplot(alpha = 0.25) + geom_point(alpha = 0.25, position = position_jitterdodge())
+dev.off()
 
 #*******************************************************************************
 # Demultiplexing ===============================================================
